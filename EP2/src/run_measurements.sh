56ade79f31b7d99f17b7766fd5b1acb63ac74674
ph <ph@retinavision.com.br>
2021-07-25 13:14:50 -0300

    fixed run measu for ompi with right size and allow root for sudo command and removed 64 processes because was not working on my computer

--- EP2/src/run_measurements.sh
+++ EP2/src/run_measurements.sh
@@ -3,7 +3,7 @@
 set -o xtrace
 
 MEASUREMENTS=15
-SIZE=2048
+SIZE=4096
 MASTER_TASK=1
 THREADS=8
 
@@ -37,10 +37,9 @@ NAME='mandelbrot_ompi'
 
 mkdir -p results/$NAME
 
-    for TASKS in 1 8 16 32 64; do
+    for TASKS in 1 8 16 32; do
         let "TOTAL_TASKS = $TASKS + $MASTER_TASK"
-        perf stat -r $MEASUREMENTS mpirun -n $TOTAL_TASKS ./$NAME -0.188 -0.012 0.554 0.754 >> triple_spiral.log 2>&1
-        # mpirun -n $TOTAL_TASKS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE >> triple_spiral.log 2>&1
+        perf stat -r $MEASUREMENTS mpirun --allow-run-as-root --oversubscribe -n $TOTAL_TASKS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE >> triple_spiral.log 2>&1
     done
 
 mv *.log results/$NAME
