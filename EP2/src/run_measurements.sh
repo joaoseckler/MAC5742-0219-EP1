edc2b9ec1db6beae1c89120399b1fb5635360091
João Seckler <jseckler@riseup.net>
2021-07-27 22:26:20 -0300

    Update run_measurements.sh

--- EP2/src/run_measurements.sh
+++ EP2/src/run_measurements.sh
@@ -22,7 +22,7 @@ rm output.ppm
 NAMES=('mandelbrot_pth' 'mandelbrot_omp')
 
 for NAME in ${NAMES[@]}; do
-    mkdir results/$NAME
+    mkdir -p results/$NAME
 
     perf stat -r $MEASUREMENTS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE $THREADS >> triple_spiral.log 2>&1
 
@@ -33,16 +33,18 @@ done
 
 
 ########### OMPI ###########
-NAME='mandelbrot_ompi'
+NAMES=('mandelbrot_ompi' 'mandelbrot_ompi_omp' 'mandelbrot_ompi_pth')
 
-mkdir -p results/$NAME
+for NAME in "${NAMES[@]}"; do
+  mkdir -p results/$NAME
 
-    for TASKS in 1 8 16 32; do
-        let "TOTAL_TASKS = $TASKS + $MASTER_TASK"
-        perf stat -r $MEASUREMENTS mpirun --allow-run-as-root --oversubscribe -n $TOTAL_TASKS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE >> triple_spiral.log 2>&1
-    done
+      for TASKS in 1 8 16 32; do
+          let "TOTAL_TASKS = $TASKS + $MASTER_TASK"
+          perf stat -r $MEASUREMENTS mpirun --bind-to none --allow-run-as-root --oversubscribe -n $TOTAL_TASKS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE >> triple_spiral.log 2>&1
+      done
 
-mv *.log results/$NAME
-rm -rf $NAME.dSYM
-rm output.ppm
+  mv *.log results/$NAME
+  rm -rf $NAME.dSYM
+  rm output.ppm
+done
 ########### OMPI ###########
